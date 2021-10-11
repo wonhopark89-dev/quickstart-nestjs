cc0039923f9ce6f151ac793faf939776f4ba8a2f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-11 22:58:30 +0900

    (๑꒪▿꒪)* Add nest service about update

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
@@ -34,5 +42,13 @@ export class BoardsController {
   @Delete('/:id')
   deleteBoard(@Param('id') id: string): void {
     this.boardService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') boardStatus: BoardStatus,
+  ): Board {
+    return this.boardService.updateBoardStatus(id, boardStatus);
   }
 }
--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -33,4 +33,10 @@ export class BoardsService {
   deleteBoard(id: string): void {
     this.boards.filter((board) => board.id !== id);
   }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
+  }
 }
