939f6cf0f248af290a66464a1bb3830190d63252
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-11 21:36:54 +0900

    (๑꒪▿꒪)* Add nest service about read

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,5 +24,10 @@ export class BoardsController {
   @Post('/')
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardService.getBoardById(id);
   }
 }
--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -25,4 +25,8 @@ export class BoardsService {
     this.boards.push(board);
     return board;
   }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
 }
