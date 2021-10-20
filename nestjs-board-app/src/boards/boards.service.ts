c8bff125ca6c781f5534be40e6a321fc3d849219
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-21 01:00:40 +0900

    (๑꒪▿꒪)* Add Exception on service, about CRUD

--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -37,7 +37,8 @@ export class BoardsService {
   }
 
   deleteBoard(id: string): void {
-    this.boards.filter((board) => board.id !== id);
+    const found = this.getBoardById(id);
+    this.boards.filter((board) => board.id !== found.id);
   }
 
   updateBoardStatus(id: string, status: BoardStatus): Board {
