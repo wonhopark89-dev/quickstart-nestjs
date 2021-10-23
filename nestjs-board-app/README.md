975baf634dd4acd17aea85ef021f41604d10a007
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-24 02:07:10 +0900

    (๑꒪▿꒪)* Add typeORM basic Setting

--- nestjs-board-app/README.md
+++ nestjs-board-app/README.md
@@ -3,3 +3,17 @@
 - Database: Postgresql
 - Database Viewer : pgAdmin
 ```
+
+#### install
+```
+brew install postgresql
+```
+#### postgresql service start
+```
+brew services start postgresql
+```
+#### check postgresql user
+```
+$ psql postgres
+postgres=# \du
+```
--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [],
   providers: [],
 })
