ecabd22f9d80b03ce090307c09fd68944702b603
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-27 00:36:47 +0900

    (๑꒪▿꒪)* Add class-validator and validationPipe

--- nestjs-board-app/src/auth/dto/auth-credential.dto.ts
+++ nestjs-board-app/src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,17 @@
+// https://github.com/typestack/class-validator
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only accepts english and number',
+  })
   password: string;
 }
