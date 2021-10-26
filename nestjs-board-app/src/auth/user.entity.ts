bf7a49089a0c7e56a537266e1553cef74cbfe3f5
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-27 00:51:56 +0900

    (๑꒪▿꒪)* Add Column unique option and error control

--- nestjs-board-app/src/auth/user.entity.ts
+++ nestjs-board-app/src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username']) // 기본값 : Controller 레벨에서 500 에러 발생/리턴
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,12 +1,26 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // debugging 을 통해 code 값 확인
+        throw new ConflictException('Exising username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
