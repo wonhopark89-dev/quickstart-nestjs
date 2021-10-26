55008ebdb26acfef94c89eed20fbcd787858084a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-27 00:27:29 +0900

    (๑꒪▿꒪)* Add signUp with DTO

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,5 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
