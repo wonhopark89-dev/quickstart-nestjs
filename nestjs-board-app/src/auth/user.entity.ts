9f7988226c128a7a800fa66de27f0441430ff858
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-29 01:11:23 +0900

    (๑꒪▿꒪)* Define relationship, between UserEntity and BoardEntity

--- nestjs-board-app/src/auth/user.entity.ts
+++ nestjs-board-app/src/auth/user.entity.ts
@@ -2,9 +2,11 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
+import { Board } from '../boards/board.entity';
 
 @Entity()
 @Unique(['username']) // 기본값 : Controller 레벨에서 500 에러 발생/리턴
@@ -17,4 +19,10 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // eager : true -> user 를 가져올때 board 정보도 가져올 수 있도록
+  // one-to-many : 한 사람이 여러 board 를 만들 수 있음
+  // Board-Entity 에서 User-Entity 를 연동해놔야 된다.
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
--- nestjs-board-app/src/boards/board.entity.ts
+++ nestjs-board-app/src/boards/board.entity.ts
@@ -1,5 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Board extends BaseEntity {
@@ -14,4 +21,7 @@ export class Board extends BaseEntity {
 
   @Column()
   status: BoardStatus;
+
+  @ManyToOne((type) => User, (user) => user.boards, { eager: false })
+  user: User;
 }
