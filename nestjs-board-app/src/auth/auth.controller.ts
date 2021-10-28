a33c44ab3ec19c3863a0407a6e6275250cec2397
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-29 00:57:04 +0900

    (๑꒪▿꒪)* Create custom decorator

--- nestjs-board-app/src/auth/auth.controller.ts
+++ nestjs-board-app/src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import {
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -29,9 +31,10 @@ export class AuthController {
   }
 
   // UseGuards 안에 @nestjs/passport 에서 가져온 AuthGuard() 를 이용하면 요청안에 유저 정보를 넣어줄 수 있습니다.
+  // req.user 가 아닌 user 라는 파라미터로 가져올 수 있는 방법 => 커스텀 데코데이터 이용
   @Post('/test')
   @UseGuards(AuthGuard())
-  test(@Req() req) {
-    // console.log('req', req);
+  test(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
