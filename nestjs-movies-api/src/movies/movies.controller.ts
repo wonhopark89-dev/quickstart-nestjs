da03710bd73ea6809eba3eeeb1888e53bf5911d0
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-11-06 23:56:16 +0900

    (ﾉ> ◇ <)ﾉ Add Service with CRUD

--- nestjs-movies-api/src/movies/movies.controller.ts
+++ nestjs-movies-api/src/movies/movies.controller.ts
@@ -8,34 +8,33 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
-  @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
+  constructor(private readonly moviesService: MoviesService) {}
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie mode after: ${searchingYear}`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('/:id')
-  getOne(@Param('id') id: string) {
+  getOne(@Param('id') id: string): Movie {
     // Param 이름은 같아야함
-    return `This will return one movie with id: ${id}`;
+    return this.moviesService.getOne(id);
   }
 
   @Post()
   create(@Body() movieData) {
     // NestJS 는 JSON 포맷을 자동으로 인식할 수 있고, 리턴도 가능하다.
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with id ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   // Put vs Patch ( Patch 는 일부분만 업데이트 )
