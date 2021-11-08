bba4edaafa6980d6471330ef5942c003bfef3271
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-11-09 00:40:03 +0900

    (ﾉ> ◇ <)ﾉ Add Unit Test on NestJS

--- nestjs-movies-api/src/movies/movies.service.spec.ts
+++ nestjs-movies-api/src/movies/movies.service.spec.ts
@@ -13,6 +13,10 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  afterAll(() => {
+    // todo : improve
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
@@ -82,6 +86,28 @@ describe('MoviesService', () => {
       });
       const afterCreate = service.getAll().length;
       expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
